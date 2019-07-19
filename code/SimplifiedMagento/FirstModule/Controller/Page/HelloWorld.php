b20acd115b00925e6e90f53cc89d91028bf1df07
udhayasuresh <udhaya91raj@gmail.com>
2019-07-19 19:08:53 +0530

    Update HelloWorld.php

--- code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -9,15 +9,61 @@ class HelloWorld extends \Magento\FrameWork\App\Action\Action
     
     public function execute()
     {
-        echo '<h1>Booking page</h1>
+        echo '<style>
+        input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
 
-<form>
-    <input name="firstname" type="text">
-    <input name="lastname" type="text">
-    <input name="phone" type="text">
-    <input name="bookingTime" type="date">
-    <input type="submit" value="Send booking informations">
-</form>';
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+<div class="container">
+<h1>Enquiry Form</h1>
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>';
     }
     
 }
